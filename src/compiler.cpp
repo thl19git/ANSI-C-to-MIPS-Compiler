fe9c4c9c3841cde89c70f3c772e8c00a4068daf0
Tom Lewis <thomas.lewis19@imperial.ac.uk>
2021-03-14 16:52:03 +0000

    open output file in compiler

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,21 +1,29 @@
 #include "../include/ast.hpp"
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 Node* parseAST();
 
 extern void yyset_in(FILE *fd);
 
-int x;
+std::ofstream output;
 
 int main(int argc, char* argv[]){
 
     std::cout << "Grabbing input file" << std::endl;
 
     FILE* sourceFile;
+
     sourceFile = fopen(argv[2],"r");
 
     if(sourceFile==nullptr){
         throw std::runtime_error("Unable to open source file");
+    }
+
+    output.open(argv[4]);
+
+    if(!output.is_open()){
+        throw std::runtime_error("Unable to open output file");
     }
 
     yyset_in(sourceFile);
@@ -27,6 +35,9 @@ int main(int argc, char* argv[]){
     std::cout << "Printing AST" << std::endl;
 
     ast->print();
+    //ast->printASM();
 
-    std::cout << "Printed AST, ending program" << std::endl; 
+    std::cout << "Printed AST, ending program" << std::endl;
+
+    output.close();
 }
