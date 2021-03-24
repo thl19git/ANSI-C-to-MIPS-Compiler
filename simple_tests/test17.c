da7e29c3ccb5456cb1e3513a5227854034bc6c48
Tom Lewis <thomas.lewis19@imperial.ac.uk>
2021-03-24 20:37:01 +0000

    adding tests

---
+++ simple_tests/test17.c
@@ -0,0 +1,8 @@
+int f(int a, int b){
+    if(a>b){
+        int x = 3;
+        a = x+a;
+    }
+    int x = 2;
+    return x + a;
+}
---
+++ simple_tests/test18.c
@@ -0,0 +1,26 @@
+int f(int a, int b, int c, int d, int e){
+    
+    int x = 1;
+    
+    x += a;
+    
+    if(b){
+        b = 5;
+    } else {
+        b = 2;
+    }
+
+    c += a * b;
+
+    for(int i = 0; i < 10; i+=1){
+        d += c;
+    }
+
+    while(e>0){
+        int y = 0x1;
+        x += d;
+        e -= y;
+    }
+    
+    return x;
+}
